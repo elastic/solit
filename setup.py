cf71b62b1bcbeb5e1ec9fff6ff2a023d3d1c4a59
Nick Lang <nick@nicklang.com>
2017-12-01 14:50:11 -0700

    bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='solit',
-      version='0.3',
+      version='0.4',
       description='The Logstash Integration Test framework',
       long_description=readme(),
       classifiers=[
